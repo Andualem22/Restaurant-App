3f382cb4708c88fc4d468e6edbea9c2e3d22b38e
Andualem <andualemderbe28@gmail.com>
2023-10-04 21:26:03 -0400

    hide info

--- src/main/java/connection/JDBConnection.java
+++ src/main/java/connection/JDBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class JDBConnection {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/restaurant_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "14@ANdy#";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL,USER,PASSWORD);
